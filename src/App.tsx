2177616a9bb1de08bee5836ef9f804cc83c3ea53
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-06-23 21:02:40 -0300

    🗃️ Estruturando os components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
-
 function App() {
+    return (
+      <div>
 
-  const {data} = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-  
-  return (
-    <ul>
-      {data?.lessons.map(lesson => {
-          return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
-  )
+      </div>
+    )
 }
 
-export default App
-
+export default App;
